38b38f82c8f67681c5f37dd509273f80794dead0
ishida_d <ishida_d@vitallead.co.jp>
2021-06-19 06:05:36 +0900

    httpsノード追加

--- xembook_config.js
+++ xembook_config.js
@@ -17,6 +17,7 @@ const NODES = [
 "https://dhealth.vistiel-arch.jp:3001",
 "https://dhealth.tokyo:3001",
 "https://dhealth.harvesting-sweet-potatoes.club:3001",
+"https://dhealth-lt.nuaka.net:3001",
 
 ].filter(word => !NG_NODES.includes(word));
 
